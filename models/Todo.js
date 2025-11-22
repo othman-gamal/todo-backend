0f885e9bed2f7bf8d03f8ed897f5f2ad721f9102
Othman Gamal <105444446+othman-gamal@users.noreply.github.com>
2025-11-22 21:04:41 +0200

    Delete models directory

--- models/Todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const todoSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Todo", todoSchema);
